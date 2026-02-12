f5fdb062526c73979d300865f120acc0e75a8e1a
Billy Ribeiro <billy.ribeiro@icloud.com>
2026-02-12 09:55:38 -0500

    Patch

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  allowedDevOrigins: ["127.0.0.1", "localhost"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "img.clerk.com" },
@@ -11,6 +12,7 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "2mb",
+      allowedOrigins: ["localhost:3000", "127.0.0.1:50325"],
     },
   },
 };
